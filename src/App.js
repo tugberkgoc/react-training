0da79795c16917d5ae3d1cfb393550c5ec2037bf
tugberkgoc <tugberkgoc@gmail.com>
2019-06-11 16:38:01 +0300

    Initial Commit :fire:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isShow: true,
+  }
+
+  componentWillUpdate(nextProps, nextState, nextContext) {
+    console.log('componentWillUpdate')
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    // console.log(`shouldComponentUpdate`)
+    console.log(nextProps, nextState, nextContext)
+
+    return true
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(`componentDidUpdate`)
+  }
+
+  render() {
+    const {isShow} = this.state
+
+    console.log('render')
+
+    return (
+        <div>
+          {isShow.toString()}
+          <button onClick={() => {
+            this.setState({isShow: !isShow})
+          }}>Toggle
+          </button>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/App2.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './App.css'
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor')
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  /**
+   *  If we remove this component(Counter) from DOM, this console.log('counter') will continue to run
+   */
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.timer = setInterval(() => { // So, we can assign variable, then delete when componentWillUnmount
+      console.log('counter')
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+    clearInterval(this.timer)
+  }
+
+  render() {
+    console.log('render')
+    return <div>Hello World!</div>
+  }
+
+}
+
+class App extends React.Component {
+
+  state = {
+    isShow: true,
+  }
+
+  render() {
+    const {isShow} = this.state
+
+    return (
+        <div>
+          {isShow && <Counter/>}
+          <button onClick={() => {
+            this.setState({isShow: false})
+          }}>Remove Counter
+          </button>
+        </div>
+    )
+  }
+
+}
+
+export default App2
